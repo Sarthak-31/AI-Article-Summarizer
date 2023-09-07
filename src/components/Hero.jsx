0d65b7494fd5fa6fb30b777455a70dd51ae34862
Sarthak-31 <103916528+Sarthak-31@users.noreply.github.com>
2023-09-07 22:34:16 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Sarthak-31/AI-Article-Summarizer", "_blank")
           }
           className='black_btn'
         >
